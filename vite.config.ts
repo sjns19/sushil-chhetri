6fb25a2622a9a59b10f1ee8100d48800d9095af5
Sujan Shrestha <sjns@Sujans-MacBook-Pro.local>
2025-12-20 16:51:03 +0545

    Change to build directory

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'docs' // Change 'build' to your desired folder name
+  },
   plugins: [
     react({
       babel: {
