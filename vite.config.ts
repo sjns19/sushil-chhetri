9d682ab6e46b2a51bca7e805468c15719762b3cd
Sujan Shrestha <sjns@Sujans-MacBook-Pro.local>
2025-12-20 16:58:38 +0545

    Fixing the usual react build mess

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   build: {
     outDir: 'docs' // Change 'build' to your desired folder name
   },
